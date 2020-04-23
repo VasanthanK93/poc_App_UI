82abb6f341649dcdcf1510c3314e889e8ad9a05a
Kurian, Richa-CW <Richa.Kurian@bestbuy.com>
2020-04-23 17:07:39 +0530

    Multiple Select Dropdown added

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,18 +2,44 @@ import React, { Component} from 'react';
 import MaterialTable from 'material-table';
 import { connect } from 'react-redux';
 import { getUsersList, editUser, deleteUser } from '../../actions/index';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
 class Users extends Component {
   constructor(props) {
     super(props);
+    const teamList = [ 'T1', 'T2','T3','T4','T5' ];
     this.state = {
       columns: [
         { title: 'Username', field: 'userName', editable: 'never' },
         {
           title: 'Role',
           field: 'role',
-          lookup: { 'DM': 'DM', 'TeamMember': 'Team Member' },
+          lookup: { 'admin': 'Admin', 'TeamMember': 'Team Member' },
         },
-        { title: 'Teams', field: 'teams' }
+        { title: 'Teams',
+          field: 'teams',
+          //lookup: { 'T1': 'T1', 'T2': 'T2', 'T3': 'T3','T4': 'T4','T5': 'T5' },
+          render: rowData => (
+            rowData.teams.join(', ')
+          ),
+          editComponent: props => (
+            <Select
+              multiple
+              value={props.rowData.teams}
+              onChange={(e)=> props.onChange(e.target.value)}
+              renderValue={selected => selected.join(', ')}
+            >
+              {teamList.map(team => (
+                <MenuItem key={team} value={team}>
+                  <Checkbox checked={props.rowData.teams.indexOf(team) > -1} />
+                  <ListItemText primary={team} />
+                </MenuItem>
+              ))}
+            </Select>
+          )
+       }
       ],
       data: [],
       loading: false
